c90a3778d588683630ba2e21d50110f7d1c86aab
anshul-59 <anshul.bansod5901@gmail.com>
2024-01-06 17:45:25 +0530

    Creating  modal that  is displayed when any of the input field in new project is empty and styling of the modalis done using tailwind CSS

--- src/components/NoProjectSelected.jsx
+++ src/components/NoProjectSelected.jsx
@@ -1,21 +1,22 @@
-import NoProjectImg from "../assets/no-projects.png";
+import noProjectImage from "../assets/no-projects.png";
 import Button from "./Button.jsx";
 
 export default function NoProjectSelected({ onStartAddProject }) {
   return (
-    <div className="mt-8 text-center w-2/3">
+    <div className="mt-24 text-center w-2/3">
       <img
-        src={NoProjectImg}
-        className="w-16 h-16 objext-contain mx-auto"
-      ></img>
-      <h2 className="text-xl font-bold text-stone-600 my-4">
-        NO PROJECT SELECTED
+        src={noProjectImage}
+        alt="An empty task list"
+        className="w-16 h-16 object-contain mx-auto"
+      />
+      <h2 className="text-xl font-bold text-stone-500 my-4">
+        No Project Selected
       </h2>
-      <p className="text-stone-400 mb-4 ">
-        Select from Sidebar or Create New Project
+      <p className="text-stone-400 mb-4">
+        Select a project or get started with a new one
       </p>
       <p className="mt-8">
-        <Button onClick={onStartAddProject}>CREATE NEW PROJECT</Button>
+        <Button onClick={onStartAddProject}>Create new project</Button>
       </p>
     </div>
   );
--- src/components/ProjectsSidebar.jsx
+++ src/components/ProjectsSidebar.jsx
@@ -4,15 +4,15 @@ export default function ProjectsSidebar({ onStartAddProject, projects }) {
   return (
     <aside className="w-1/3 px-8 py-16 bg-stone-900 text-stone-50 md:w-72 rounded-r-xl">
       <h2 className="mb-8 font-bold uppercase md:text-xl text-stone-200">
-        YOUR PROJECTS
+        Your Projects
       </h2>
       <div>
-        <Button onClick={onStartAddProject}>+ ADD PROJECT</Button>
+        <Button onClick={onStartAddProject}>+ Add Project</Button>
       </div>
       <ul className="mt-8">
         {projects.map((project) => (
           <li key={project.id}>
-            <button className="w-full mt-2 text-left px-4 py-2 rounded-md text-stone-300 hover:text-stone-100 hover:bg-stone-600">
+            <button className="w-full text-left px-2 py-1 rounded-sm my-1 text-stone-400 hover:text-stone-200 hover:bg-stone-800">
               {project.title}
             </button>
           </li>
