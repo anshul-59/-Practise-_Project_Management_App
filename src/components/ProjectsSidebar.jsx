3cfd4ea06bd447fa8ceb81ba4147f85b019e06f9
anshul-59 <anshul.bansod5901@gmail.com>
2024-01-06 16:51:34 +0530

    updating UI on sidebar and making the save button functional.

--- src/components/ProjectsSidebar.jsx
+++ src/components/ProjectsSidebar.jsx
@@ -1,6 +1,6 @@
 import Button from "./Button.jsx";
 
-export default function ProjectsSidebar({ onStartAddProject }) {
+export default function ProjectsSidebar({ onStartAddProject, projects }) {
   return (
     <aside className="w-1/3 px-8 py-16 bg-stone-900 text-stone-50 md:w-72 rounded-r-xl">
       <h2 className="mb-8 font-bold uppercase md:text-xl text-stone-200">
@@ -9,7 +9,15 @@ export default function ProjectsSidebar({ onStartAddProject }) {
       <div>
         <Button onClick={onStartAddProject}>+ ADD PROJECT</Button>
       </div>
-      <ul></ul>
+      <ul className="mt-8">
+        {projects.map((project) => (
+          <li key={project.id}>
+            <button className="w-full mt-2 text-left px-4 py-2 rounded-md text-stone-300 hover:text-stone-100 hover:bg-stone-600">
+              {project.title}
+            </button>
+          </li>
+        ))}
+      </ul>
     </aside>
   );
 }
