99c2e0c7f8e3b08a17b07ea575721afa8381cdac
anshul-59 <anshul.bansod5901@gmail.com>
2024-01-06 16:17:54 +0530

    saving the user value to the object created.

--- src/components/NoProjectSelected.jsx
+++ src/components/NoProjectSelected.jsx
@@ -1,3 +1,22 @@
-export default function NoProjectSelected() {
-  return;
+import NoProjectImg from "../assets/no-projects.png";
+import Button from "./Button.jsx";
+
+export default function NoProjectSelected({ onStartAddProject }) {
+  return (
+    <div className="mt-8 text-center w-2/3">
+      <img
+        src={NoProjectImg}
+        className="w-16 h-16 objext-contain mx-auto"
+      ></img>
+      <h2 className="text-xl font-bold text-stone-600 my-4">
+        NO PROJECT SELECTED
+      </h2>
+      <p className="text-stone-400 mb-4 ">
+        Select from Sidebar or Create New Project
+      </p>
+      <p className="mt-8">
+        <Button onClick={onStartAddProject}>CREATE NEW PROJECT</Button>
+      </p>
+    </div>
+  );
 }
